4f994414c7da70dcc8595ed24039a08e29c8752a
amabdelftah <ahmedabdelftah95165@gmail.com>
2023-04-03 09:54:27 +0200

    Add laravel debugbar and laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::post('posts', [PostController::class, 'store'])->name('posts.store');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
